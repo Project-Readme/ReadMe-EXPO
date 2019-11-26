67bfaa461319115d087b362b32c839a2625103b2
Daniel <djumelle@oldwestbury.edu>
2019-11-26 18:00:09 -0500

    Finished home page design

--- screens/Home.js
+++ screens/Home.js
@@ -1,15 +1,58 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView } from 'react-native';
 import TopBar from '../components/topBar';
 import styles from '../styles'
 
+import Card from '../components/Card'
+import { FlatList } from 'react-native-gesture-handler';
+import ArticleCard from '../components/ArticleCard';
 
+const dummy = [
+    {
+        title: 'How to Build a Simple Chrome Extension in Vanilla JavaScript',
+        img: { uri: "https://miro.medium.com/max/2957/1*HwO6wiOHiJrN1_jePQrmEA.jpeg" }
+    },
+    {
+        title: 'How to Perform Web-Scraping using Node.js',
+        img: { uri: "https://miro.medium.com/max/4592/1*tBBX7RGFkadc_yiShZLDCQ.jpeg" }
+    },
+    {
+        title: 'You Don’t Understand Bitcoin Because You Think Money Is Real',
+        img: { uri: "https://miro.medium.com/max/3887/1*1DQEeByasuoteYxoySd5SA.jpeg" }
+    },
+    {
+        title: 'The Real Cost of Phone Addiction',
+        img: { uri: "https://miro.medium.com/max/2400/1*wMjnTSs_-znQ2NRUjysK4w.png" }
+    },
+]
 const Home = props => {
     return (
-        <View style={styles.homeContainer}>
+        <ScrollView style={styles.homeContainer}>
             <TopBar />
-            <Text>Home</Text>
-        </View>
+            <Text style={{ color: "#747882", padding: 10, paddingBottom: 0, fontSize: 24, fontWeight: "bold" }}>Most Popular</Text>
+            <ScrollView
+                horizontal={true}
+                style={{ paddingBottom: 20 }}
+                showsHorizontalScrollIndicator={false}
+            >
+                {dummy.map((item, idx) => <Card key={idx} title={item.title} image={item.img}></Card>)}
+            </ScrollView>
+            <Text style={{ color: "#747882", padding: 10, paddingBottom: 0, fontSize: 24, fontWeight: "bold" }}>Your Articles</Text>
+
+            <FlatList
+                keyExtractor={article => article.title}
+                data={dummy}
+                renderItem={article => {
+
+                    return (
+                        <View style={{ borderColor: 'black', borderBottomWidth: 1, paddingTop: 10, paddingBottom: 5 }}>
+                            <ArticleCard image={article.item.img} text={article.item.title}></ArticleCard>
+                        </View>
+                    )
+                }}
+            >
+            </FlatList>
+        </ScrollView>
     )
 }
 
