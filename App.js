0c56de1e93a33d7f55542e13334f5528ac8e5423
Bryce G <bryceagarbutt@gmail.com>
2019-11-25 23:32:35 -0500

    Expo & Firestore WORKING

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import db from './database';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ database/index.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase';
+import '@firebase/firestore';
+import secrets from './secrets';
+
+firebase.initializeApp(secrets);
+let db = firebase.firestore();
+
+let test = db.collection('content').doc('newArticle');
+let newArticle = test
+  .get()
+  .then(doc => {
+    if (!doc.exists) {
+      console.log('No such document!');
+    } else {
+      console.log('Document data:', doc.data());
+    }
+  })
+  .catch(err => {
+    console.log('Error getting document', err);
+  });
+console.log(newArticle);
+export default db;
