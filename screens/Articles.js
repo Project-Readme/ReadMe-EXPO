d5a5a85b01e6dd167a6bb64144720f0fea987d37
SimonG <saguti15@g.holycross.edu>
2019-11-27 15:08:10 -0500

    fix: took off top blank bar from home page

--- screens/Articles.js
+++ screens/Articles.js
@@ -3,7 +3,8 @@ import { connect } from 'react-redux';
 import { Text, View, ScrollView, Image, TouchableOpacity, Button } from 'react-native';
 import TopBar from '../components/topBar';
 import Card from '../components/Card';
-import { setCurrentContent } from '../store/currentContent'
+import { setCurrentContent } from '../store/currentContent';
+import styles from '../styles';
 
 class AllArticles extends React.Component {
     static navigationOptions = {
@@ -13,26 +14,28 @@ class AllArticles extends React.Component {
     render() {
         const { navigate } = this.props.navigation;
         return (
-          <ScrollView>
-              <TopBar />
-             { this.props.contentList.map((article, index) => (
-                        <TouchableOpacity
-                        key={index}
-                        onPress={
-                            () => {
-                                this.props.setCurrentContent(article);
-                                navigate('Article');
+            <View >
+                <TopBar />
+                <ScrollView>
+                { this.props.contentList.map((article, index) => (
+                            <TouchableOpacity
+                            key={index}
+                            onPress={
+                                () => {
+                                    this.props.setCurrentContent(article);
+                                    navigate('Article');
+                                }
                             }
-                        }
-                        >
-                            <Card
-                                title={article.title}
-                                image={{uri: 'https://miro.medium.com/max/2400/1*wMjnTSs_-znQ2NRUjysK4w.png'}}
-                            />
-                        </TouchableOpacity>
-             ))
-              }
-          </ScrollView>
+                            >
+                                <Card
+                                    title={article.title}
+                                    image={{uri: 'https://miro.medium.com/max/2400/1*wMjnTSs_-znQ2NRUjysK4w.png'}}
+                                />
+                            </TouchableOpacity>
+                ))
+                }
+                </ScrollView>
+            </View>
         );
       }
     }
