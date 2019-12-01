b2773155e5a2736c0a8c319aba717699b2be91a9
Sarah Trefethen <49411879+sarahtrefethen@users.noreply.github.com>
2019-12-01 15:49:02 -0500

    installed and set up redux-persist. works for persisting logged in user.

--- store/index.js
+++ store/index.js
@@ -1,20 +1,30 @@
 import thunkMiddleware from 'redux-thunk'
 import { createLogger } from 'redux-logger'
 import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import {AsyncStorage} from 'react-native';
 
 import currentContentReducer from './currentContent';
 import contentListReducer from './contentList';
 import mostPopularReducer from './mostPopularList';
 import userReducer from './user';
 
-// const logger = createLogger();
+const logger = createLogger();
 
-const reducers = combineReducers({
+const rootReducer = combineReducers({
     currentContent: currentContentReducer,
     contentList: contentListReducer,
     mostPopularList: mostPopularReducer,
     user: userReducer
 });
 
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage
+  }
+  
+  const persistedReducer = persistReducer(persistConfig, rootReducer)
+  
+export const store = createStore(persistedReducer, applyMiddleware(thunkMiddleware, logger));
+export const persistor = persistStore(store)
 
-export default createStore(reducers, applyMiddleware(thunkMiddleware));
