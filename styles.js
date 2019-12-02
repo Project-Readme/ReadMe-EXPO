2b3393faabdd2f2668fdbd47f148687bca4bc254
Daniel <djumelle@oldwestbury.edu>
2019-12-02 12:18:26 -0500

    centered all articles

--- styles.js
+++ styles.js
@@ -38,50 +38,52 @@ module.exports = StyleSheet.create({
     backgroundColor: 'white'
   },
   greeting: {
-      marginBottom: 32,
-      fontSize: 18,
-      fontWeight: '400',
-      textAlign: 'center'
+    marginBottom: 32,
+    fontSize: 18,
+    fontWeight: '400',
+    textAlign: 'center'
   },
   errorMessage: {
-      height: 72,
-      alignItems: 'center',
-      justifyContent: 'center',
-      marginHorizontal: 30
-  },
-  error: {
-      color: '#E9446A',
-      fontSize: 13,
-      fontWeight: '600',
-      textAlign: 'center'
-  },
-  form: {
-      marginBottom: 48,
-      marginHorizontal: 30
-  },
-  inputTitle: {
-      color: '#8A8F9E',
-      fontSize: 10,
-      textTransform: 'uppercase'
-  },
-  input: {
-      borderBottomColor: '#8A8F9E',
-      borderBottomWidth: StyleSheet.hairlineWidth,
-      height: 40,
-      fontSize: 15,
-      color: '#161F3D'
-  },
-  button: {
-      marginHorizontal: 30,
-      backgroundColor: '#E9446A',
-      borderRadius: 4,
-      height: 52,
-      alignItems: 'center',
-      justifyContent: 'center',
-  },
-  AllArticles: {
-    flex: 1,
+    height: 72,
     alignItems: 'center',
     justifyContent: 'center',
+    marginHorizontal: 30
+  },
+  error: {
+    color: '#E9446A',
+    fontSize: 13,
+    fontWeight: '600',
+    textAlign: 'center'
+  },
+  form: {
+    marginBottom: 48,
+    marginHorizontal: 30
+  },
+  inputTitle: {
+    color: '#8A8F9E',
+    fontSize: 10,
+    textTransform: 'uppercase'
+  },
+  input: {
+    borderBottomColor: '#8A8F9E',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    height: 40,
+    fontSize: 15,
+    color: '#161F3D'
+  },
+  button: {
+    marginHorizontal: 30,
+    backgroundColor: '#E9446A',
+    borderRadius: 4,
+    height: 52,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  AllArticles: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  AllArticlesContainer: {
+    flex: 1
   }
 });
