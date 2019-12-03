8f4ba99501221c03a6f3cbe59a1358f5fb5f7dac
Daniel <djumelle@oldwestbury.edu>
2019-12-03 11:01:20 -0500

    added search functionality and rendering

--- screens/Home.js
+++ screens/Home.js
@@ -12,82 +12,50 @@ import Card from '../components/Card';
 import { FlatList } from 'react-native-gesture-handler';
 import ArticleCard from '../components/ArticleCard';
 
-const dummy = [
-    {
-        title: 'How to Build a Simple Chrome Extension in Vanilla JavaScript',
-        img: { uri: 'https://miro.medium.com/max/2957/1*HwO6wiOHiJrN1_jePQrmEA.jpeg' }
-    },
-    {
-        title: 'How to Perform Web-Scraping using Node.js',
-        img: { uri: 'https://miro.medium.com/max/4592/1*tBBX7RGFkadc_yiShZLDCQ.jpeg' }
-    },
-    {
-        title: 'You Don’t Understand Bitcoin Because You Think Money Is Real',
-        img: { uri: 'https://miro.medium.com/max/3887/1*1DQEeByasuoteYxoySd5SA.jpeg' }
-    },
-    {
-        title: 'The Real Cost of Phone Addiction',
-        img: { uri: 'https://miro.medium.com/max/2400/1*wMjnTSs_-znQ2NRUjysK4w.png' }
-    },
-]
 class Home extends React.Component {
     componentDidMount() {
 
         checkInternetConnection().then(isConnected => {
-          this.props.connectionChange(isConnected);
-          console.log(isConnected)
-          if (isConnected && this.props.user) {
-            this.props.loadContentList(this.props.user);
-            this.props.loadMostPopular();
-          }
+            this.props.connectionChange(isConnected);
+            if (isConnected && this.props.user) {
+                this.props.loadContentList(this.props.user);
+                this.props.loadMostPopular();
+            }
         })
-      }
+    }
 
     render() {
-   const { navigate } = this.props.navigation;
-   return (
-        <View style={styles.homeContainer}>
-        <TopBar />
-            <Text style={{ color: '#747882', padding: 10, paddingBottom: 0, fontSize: 24, fontWeight: 'bold' }}>Most Popular</Text>
-            <ScrollView
-                horizontal={true}
-                style={{ paddingBottom: 20 }}
-                showsHorizontalScrollIndicator={false}
-            >
-                {this.props.mostPopularList.map((item) => (
-                <TouchableOpacity
-                key={item.id}
-                onPress={
-                    () => {
-                        this.props.setCurrentContent(item);
-                        navigate('Article');
-                    }
-                }
+        const { navigate } = this.props.navigation;
+        return (
+            <View style={styles.homeContainer}>
+                <TopBar />
+                <Text style={{ color: '#747882', padding: 10, paddingBottom: 0, fontSize: 24, fontWeight: 'bold' }}>Most Popular</Text>
+                <ScrollView
+                    horizontal={true}
+                    style={{ paddingBottom: 20 }}
+                    showsHorizontalScrollIndicator={false}
                 >
-                <Card
-                title={item.title}
-                image={{ uri: item.image }}
-                />
-                </TouchableOpacity>
-                ))}
-            </ScrollView>
-            <Text style={{ color: '#747882', padding: 10, paddingBottom: 0, fontSize: 24, fontWeight: 'bold' }}>Recommended</Text>
+                    {this.props.mostPopularList.map((item) => (
+                        <TouchableOpacity
+                            key={item.id}
+                            onPress={
+                                () => {
+                                    this.props.setCurrentContent(item);
+                                    navigate('Article');
+                                }
+                            }
+                        >
+                            <Card
+                                title={item.title}
+                                image={{ uri: item.image }}
+                            />
+                        </TouchableOpacity>
+                    ))}
+                </ScrollView>
+                <Text style={{ color: '#747882', padding: 10, paddingBottom: 0, fontSize: 24, fontWeight: 'bold' }}>Recent Articles</Text>
 
-            <FlatList
-                keyExtractor={article => article.title}
-                data={dummy}
-                renderItem={article => {
-
-                    return (
-                        <View style={{ borderColor: 'black', borderBottomWidth: 1, paddingTop: 10, paddingBottom: 5 }}>
-                            <ArticleCard image={article.item.img} text={article.item.title} />
-                        </View>
-                    )
-                }}
-             />
-
-        </View>
-    )
+            </View>
+        )
     }
 }
 
@@ -99,8 +67,8 @@ const mapStateToProps = state => {
     return {
         mostPopularList: state.mostPopularList,
         user: state.user.email
-        }
-    };
+    }
+};
 
 const mapDispatchToProps = dispatch => ({
     setCurrentContent: (article) => dispatch(setCurrentContent(article)),
