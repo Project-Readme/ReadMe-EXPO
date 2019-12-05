035452b1b92c5528eeacc30e86a259f4de1d4154
Bryce G <bryceagarbutt@gmail.com>
2019-12-04 19:43:44 -0500

    IT WORKS

--- navigator/SwitchNavigator.js
+++ navigator/SwitchNavigator.js
@@ -1,10 +1,10 @@
-import Home from '../screens/Home'
-import Articles from '../screens/Articles'
-import User from '../screens/User'
-import Login from '../screens/Login'
-import Signup from '../screens/Signup'
-import Loading from '../screens/Loading'
-import Search from '../screens/Search'
+import Home from '../screens/Home';
+import Articles from '../screens/Articles';
+import User from '../screens/User';
+import Login from '../screens/Login';
+import Signup from '../screens/Signup';
+import Loading from '../screens/Loading';
+import Search from '../screens/Search';
 
 import 'react-native-gesture-handler';
 import { createStackNavigator } from 'react-navigation-stack';
@@ -13,41 +13,36 @@ import SingleArticle from '../screens/SingleArticle';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 
-
 const AuthStack = createStackNavigator({
-    Login,
-    Signup
-  });
+  Login,
+  Signup,
+});
 
-  const HomeScreen = createStackNavigator({
-    Loading,
-    Home: Home,
-    Article: SingleArticle,
+const HomeScreen = createStackNavigator({
+  Loading,
+  Home: Home,
+  Article: SingleArticle,
+});
 
-  });
+const UserStack = createStackNavigator({
+  User: User,
+});
 
-  const UserStack = createStackNavigator({
-    User: User,
-  });
+const ArticlesStack = createStackNavigator({
+  Articles: Articles,
+  Article: SingleArticle,
+});
 
-  const ArticlesStack = createStackNavigator({
-    Articles: Articles,
-    Article: SingleArticle
-  });
+const TabNavigator = createBottomTabNavigator({
+  Home: HomeScreen,
+  Articles: ArticlesStack,
+  Search,
+  User: UserStack,
+});
 
-  const TabNavigator = createBottomTabNavigator({
-    Home: HomeScreen,
-    Articles: ArticlesStack,
-    Search,
-    User
-  });
+const AuthSwitchNavigator = createSwitchNavigator({
+  Home: TabNavigator,
+  Auth: AuthStack,
+});
 
-  const AuthSwitchNavigator = createSwitchNavigator(
-    {
-        Home: TabNavigator,
-        Auth: AuthStack,
-    }
-  )
-
- export default createAppContainer(AuthSwitchNavigator);
-
+export default createAppContainer(AuthSwitchNavigator);
