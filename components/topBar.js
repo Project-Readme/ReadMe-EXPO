8700f6805801708445193e2ccae383bf2b2ada39
SimonG <saguti15@g.holycross.edu>
2019-12-04 20:54:55 -0500

    feature: added animation for icon

--- components/topBar.js
+++ components/topBar.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import { View, Text, Image } from 'react-native';
 import styles from '../styles';
+import Animation from 'lottie-react-native';
 
-export default function() {
-  return (
-    <View style={styles.topBar}>
-      <Image
-        style={{ marginTop: 10, marginRight: 10, width: 50, height: 50 }}
-        source={require('../assets/icon.png')}
-      />
-      <Text style={{ marginTop: 10, color: 'white', fontSize: 20 }}>
-        ReadMe: Reading on the Go
-      </Text>
-    </View>
-  );
+export default class TopBar extends React.Component {
+
+  componentDidMount() {
+    this.animation.play();
+  }
+
+  render() {
+    return (
+      <View style={styles.topBar}>
+        {/* <Image
+          style={{ marginTop: 10, marginRight: 10, width: 50, height: 50 }}
+          source={require('../assets/icon.png')}
+        /> */}
+        <View>
+          <Animation
+            ref={ animation => {
+                this.animation = animation;
+              }}
+            style={{
+              width: 90,
+              height: 90,
+            }}
+            loop={true}
+            source={require('../assets/book.json')}
+          />
+        </View>
+        <Text style={{ marginTop: 10, color: 'white', fontSize: 20 }}>
+          ReadMe: Reading on the Go
+        </Text>
+      </View>
+    );
+  }
 }
