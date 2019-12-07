d34d3054aa259d3665b0cf6223251fca3f613a5a
Daniel <djumelle@oldwestbury.edu>
2019-12-06 23:35:16 -0500

    functionality for add almost finished

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, ScrollView, TouchableOpacity, RefreshControl } from 'react-native';
+import { Text, View, ScrollView, TouchableOpacity, RefreshControl, Animated } from 'react-native';
 import TopBar from '../components/topBar';
 import styles from '../styles';
 import { connect } from 'react-redux';
@@ -7,16 +7,20 @@ import { setCurrentContent } from '../store/currentContent';
 import { loadMostPopular } from '../store/mostPopularList';
 import { loadContentList } from '../store/contentList';
 import { checkInternetConnection, offlineActionCreators } from 'react-native-offline';
+// import Swipeable from 'react-native-gesture-handler/Swipeable';
+import { Entypo } from '@expo/vector-icons'
+import db from '../database';
 
 import Card from '../components/Card';
-import { FlatList } from 'react-native-gesture-handler';
+import Swipeable from 'react-native-swipeable-row';
+import { FlatList, RectButton } from 'react-native-gesture-handler';
 import ArticleCard from '../components/ArticleCard';
 
 class Home extends React.Component {
     constructor() {
         super();
         this.state = {
-            isRefreshing: false
+            isRefreshing: false,
         }
         this.onRefresh = this.onRefresh.bind(this)
     }
@@ -25,79 +29,143 @@ class Home extends React.Component {
     };
 
     onRefresh() {
-        this.setState({isRefreshing: true});
-        setTimeout( () => {
+        this.setState({ isRefreshing: true });
+        setTimeout(() => {
             try {
                 this.props.loadContentList(this.props.user.email);
                 this.props.loadMostPopular();
-                this.setState({isRefreshing: false});
+                this.setState({ isRefreshing: false });
 
             } catch (error) {
                 console.log(error)
-                this.setState({isRefreshing: false});
+                this.setState({ isRefreshing: false });
             }
         });
-      }
+    }
+
+    // updateRef = ref => {
+    //     this._swipeableRow = ref;
+    // };
+
+    // close = () => {
+    //     this._swipeableRow.close();
+    // };
+
+    leftActions = (
+        <TouchableOpacity style={styles.swipeableAdd} onPress={() => {
+            alert('added')
+            // this.addArticle(article.item)
+        }}>
+
+            <View>
+                <Entypo name="add-to-list" size={36} style={{ paddingTop: '1%' }}></Entypo>
+            </View>
+        </TouchableOpacity>
+    )
+
+
+    // async addArticle(article) {
+    //     const url = article.url.split('/').join('')
+
+    //     try {
+    //         const article = await db.collection('articles').doc(url).get()
+    //         const data = article.data()
+    //         const ref = await db.collection('users').doc(this.props.user).collection('articles').doc(url)
+    //         ref.set({
+    //             Title: data.Title,
+    //             Body: data.Body,
+    //             Head: data.Head,
+    //             Url: data.URL,
+    //             Image: data.Image
+    //         })
+    //         this.props.loadContentList(this.props.user)
+    //     } catch (err) {
+    //         console.log('error', err)
+    //     }
+    // }
+
+
+    // //=====================================
+    // renderLeftActions = (progress, dragX) => {
+    //     const scale = dragX.interpolate({
+    //         inputRange: [0, 80],
+    //         outputRange: [0, 1],
+    //         extrapolate: 'clamp',
+    //     });
+    //     return (
+    //         <TouchableOpacity style={styles.swipeableAdd} onPress={() => {
+    //             // this.addArticle(article.item)
+    //             this.close()
+    //         }}>
+    //             <View>
+    //                 <Entypo name="add-to-list" size={36} style={{ paddingTop: '1%' }}></Entypo>
+    //             </View>
+    //         </TouchableOpacity>
+    //     );
+    // }
+
 
     render() {
+        const { children } = this.props;
         const { navigate } = this.props.navigation;
         return (
-                <View style={styles.homeContainer}>
-                    <TopBar />
-                    <ScrollView
+            <View style={styles.homeContainer}>
+                <TopBar />
+                <ScrollView
                     refreshControl={
                         <RefreshControl refreshing={this.state.isRefreshing} onRefresh={this.onRefresh} />
                     }
+                >
+                    <Text style={styles.homeHeader}>Most Popular</Text>
+                    <ScrollView
+                        horizontal={true}
+                        style={{ paddingBottom: 20 }}
+                        showsHorizontalScrollIndicator={false}
                     >
-                        <Text style={styles.homeHeader}>Most Popular</Text>
-                        <ScrollView
-                            horizontal={true}
-                            style={{ paddingBottom: 20 }}
-                            showsHorizontalScrollIndicator={false}
-                        >
-                            {this.props.mostPopularList.map((item) => (
+                        {this.props.mostPopularList.map((item) => (
                             <TouchableOpacity
-                            key={item.id}
-                            onPress={
-                                () => {
-                                    this.props.setCurrentContent(item);
-                                    navigate('Article');
-                                }}
+                                key={item.id}
+                                onPress={
+                                    () => {
+                                        this.props.setCurrentContent(item);
+                                        navigate('Article');
+                                    }}
                             >
                                 <Card
-                                title={item.title}
-                                image={{ uri: item.image }}
+                                    title={item.title}
+                                    image={{ uri: item.image }}
                                 />
                             </TouchableOpacity>
-                            ))}
-                        </ScrollView>
-                        <Text style={styles.homeHeader}>Recent Articles</Text>
-                        <FlatList
-                            keyExtractor={article => article.title}
-                            data={this.props.mostRecentList}
-                            renderItem={article => {
-
-                                return (
-                                    <TouchableOpacity
+                        ))}
+                    </ScrollView>
+                    <Text style={styles.homeHeader}>Recent Articles</Text>
+                    <FlatList
+                        keyExtractor={article => article.title}
+                        data={this.props.mostRecentList}
+                        renderItem={article => {
+                            return (
+                                <TouchableOpacity
                                     onPress={
                                         () => {
                                             this.props.setCurrentContent(article.item);
                                             navigate('Article');
                                         }
                                     }
-                                    >
+                                >
                                     <View style={styles.recentBox}>
-                                        <ArticleCard image={{uri: article.item.image}} text={article.item.title} />
+                                        <Swipeable leftContent={<Text>hi</Text>}>
+                                            <ArticleCard image={{ uri: article.item.image }} text={article.item.title} />
+                                        </Swipeable>
                                     </View>
-                                    </TouchableOpacity>
-                                )
-                            }}
-                        />
-                    </ScrollView>
-                </View>
-            )
-     }
+                                </TouchableOpacity>
+                            )
+                        }}
+                    />
+                </ScrollView>
+            </View>
+        )
     }
+}
 
 const mapStateToProps = state => {
     return {
