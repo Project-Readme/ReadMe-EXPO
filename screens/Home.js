bc3393cdcdf98bbd2de1e16251ccbd0193113e54
SimonG <saguti15@g.holycross.edu>
2019-12-08 18:56:02 -0500

    fix: users data now loads on refresh

--- screens/Home.js
+++ screens/Home.js
@@ -30,8 +30,8 @@ class Home extends React.Component {
             try {
                 checkInternetConnection().then(isConnected => {
                     this.props.connectionChange(isConnected);
-                    if (isConnected && this.props.user.email) {
-                        this.props.loadContentList(this.props.user.email);
+                    if (isConnected && this.props.user) {
+                        this.props.loadContentList(this.props.user);
                         this.props.loadMostPopular();
                     }
                 })
